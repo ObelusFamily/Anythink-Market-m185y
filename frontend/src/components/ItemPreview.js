107e5e09cadd78510575b25d035a81ce927d5733
wilcoquests <110761906+wilcoquests@users.noreply.github.com>
2022-09-12 10:55:55 +0200

    Update ItemPreview.js

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -4,7 +4,6 @@ import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
 
-
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
     dispatch({
@@ -37,7 +36,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image || process.env.PUBLIC_URL + "/placeholder.png"}
+        // src={item.image || process.env.PUBLIC_URL + "/placeholder.png"}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
