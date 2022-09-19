f9b07106d1cb7c24d09bc8448a462038251b10b6
Erik Sapir <erik.sapir@wilcohq.com>
2022-09-19 19:18:46 +0300

    fix

--- frontend/src/agent.js
+++ frontend/src/agent.js
@@ -3,7 +3,7 @@ import _superagent from "superagent";
 
 const superagent = superagentPromise(_superagent, global.Promise);
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const API_ROOT = `${BACKEND_URL}/api`;
 
